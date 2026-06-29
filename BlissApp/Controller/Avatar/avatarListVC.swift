6f5706882d4fdbc522a2cd2ea9c420d4c09cd4e7
Vignesh Krishnamurthy <77401287+vigneshkrish1989@users.noreply.github.com>
2026-06-29 16:42:18 +0530

    Add debug print statement in avatarListVC

--- BlissApp/Controller/Avatar/avatarListVC.swift
+++ BlissApp/Controller/Avatar/avatarListVC.swift
@@ -17,6 +17,7 @@ class avatarListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("Hello Avatarrr!")
         // Do any additional setup after loading the view.
         self.title = "AVATARS LIST"
         self.parse.parseUserAvatar(completion: {
